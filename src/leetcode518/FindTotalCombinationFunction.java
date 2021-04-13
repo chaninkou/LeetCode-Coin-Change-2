5932da7f5df639477c644d80dac4e400b156f9bf
chaninkou <michaelkoulove@yahoo.com>
2021-04-12 20:22:33 -0700

    Redid problem on dp.

--- src/leetcode518/FindTotalCombinationFunction.java
+++ src/leetcode518/FindTotalCombinationFunction.java
@@ -12,7 +12,7 @@ public class FindTotalCombinationFunction {
 		// Base case: there is only one way to get 0 amount
 		dp[0] = 1;
 
-		// For each coins, we update the dp for each coins
+		// For each coins, we update the dp
 		for (int coin : coins) {
 			for (int current = coin; current <= amount; current++) {
 				// current - coin will get the total amount exclude current coin
